c841e5b5d443bd3e975b08d1084dbc2d9f6e762a
marcusvxf <marcusviniciusleite1@hotmail.com>
2021-09-02 11:16:14 -0300

    configurando styled da FavoritesPage

--- src/Pages/Favorites/index.jsx
+++ src/Pages/Favorites/index.jsx
@@ -1,32 +1,27 @@
 import React, { useContext } from 'react';
 import { firstContext } from '../../Store/store';
-import {Card} from '../../components'
-import { Display, Nothing, Title } from './style';
+import {Card,Nothing} from '../../components'
+import { Display, Title } from './style';
 
 const Favorite = ()=>{
 
-    const {state} = useContext(firstContext)
+    const {state} = useContext(firstContext);
     
-
     const pokemonCard = (pokemon) => {
 
         return(
                 <Card key={pokemon.name} src={pokemon.sprites.front_default} name={pokemon.name.replace('-',' ')} id={pokemon.id} ></Card>
-        )
+        );
     }
-
     return(
         <>
-
             <Title>Pokemons Favoritos</Title>
             <Display>
                 {state.favorites.length > 0 ? state.favorites.map((pokemon)=> pokemonCard(pokemon)):<Nothing> você Não salvou nada ainda, pesquise um nome ou numero.</Nothing>}
             </Display>
-            
-
         </>
-    )
+    );
 
 }
 
-export default Favorite
+export default Favorite;
