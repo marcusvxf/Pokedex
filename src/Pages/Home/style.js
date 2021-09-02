fbf5f16978b5694a2e6698318b8c7ecdbfeef4b7
marcusvxf <marcusviniciusleite1@hotmail.com>
2021-09-02 10:19:41 -0300

    configurando styled da pagina Home

--- src/Pages/Home/style.js
+++ src/Pages/Home/style.js
@@ -1,30 +1,25 @@
 import styled from "styled-components";
 
-
-
-export const Title = styled.h1`
-    font-family: 'Helvetica Neue', sans-serif;
-    font-size:7rem;
-    text-transform:uppercase;
-
-`
-
-export const SubTitle = styled.h3`
-    font-family: 'Helvetica Neue', sans-serif;
-    font-size:1.2rem;
-    text-transform:uppercase;
-    margin:-4rem;
-    text-align:center;
-    width:60rem
-
-`
-
 export const Container = styled.div`
     height:55vh;
     padding:0 6rem 0 6rem;
     display:flex;
     flex-direction:column;
     justify-content:center;
-
     align-items:center;
-`
+
+    h1{
+        font-family: 'Helvetica Neue', sans-serif;
+        font-size:7rem;
+        text-transform:uppercase;
+    }
+
+    h3{
+        font-family: 'Helvetica Neue', sans-serif;
+        font-size:1.2rem;
+        text-transform:uppercase;
+        margin:-4rem;
+        text-align:center;
+        width:60rem
+    }
+`;
